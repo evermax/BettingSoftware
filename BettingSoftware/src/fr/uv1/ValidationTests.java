30e701ddc1f85ad409d973a93911b6a6e91690a9
Maxime <maxlasserre@free.fr>
2014-05-26 16:56:46 +0200

    Vidage de la BDD avant Tests

--- BettingSoftware/src/fr/uv1/ValidationTests.java
+++ BettingSoftware/src/fr/uv1/ValidationTests.java
@@ -1,17 +1,35 @@
 package fr.uv1;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import fr.uv1.bettingServices.*;
 import fr.uv1.bettingServices.exceptions.BadParametersException;
 import fr.uv1.tests.validation.withoutPersistence.FirstIncrementValidationTests;
 import fr.uv1.tests.validation.withoutPersistence.SecondIncrementValidationTests;
+import fr.uv1.utils.DataBaseConnection;
 
 @SuppressWarnings("unused")
 public class ValidationTests {
+    private static Connection connection;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+	    
+        PreparedStatement psRemove;
+        try {
+            connection = DataBaseConnection.getConnection();
+            psRemove = connection.prepareStatement(" DELETE FROM competitionparticipants; DELETE FROM bet; DELETE FROM teammembers; DELETE FROM competitionranking; DELETE FROM competition; DELETE FROM competitor; DELETE FROM subscriber;");
+            psRemove.executeUpdate();
+            psRemove.close();
+            connection.close();
+        } catch (SQLException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
 
 		new SecondIncrementValidationTests() {
 
