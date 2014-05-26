3d608f0e5e3d762ecf0d829d3e3fd1a2bcc28027
Maxime <maxlasserre@free.fr>
2014-05-26 15:06:51 +0200

    CompetitorPlayer amélioration du constructeur avec id

--- BettingSoftware/src/fr/uv1/bettingServices/CompetitorPlayer.java
+++ BettingSoftware/src/fr/uv1/bettingServices/CompetitorPlayer.java
@@ -28,15 +28,7 @@ public class CompetitorPlayer extends ACompetitor {
     
     public CompetitorPlayer(int id, String lastName, String firstName, Calendar borndate)
             throws BadParametersException {
-        super(lastName);
-        if (firstName == null || borndate == null) {
-            throw new BadParametersException();
-        }
-        if (!name.matches(REGEX_NAME) || !firstName.matches(REGEX_NAME)) {
-            throw new BadParametersException();
-        }
-        this.firstName = firstName;
-        this.borndate = borndate;
+        this(lastName, firstName, borndate);
         this.id = id;
     }
 
